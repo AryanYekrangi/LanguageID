ffb6a407a2485f7ca6850835486311578668eee5
Aryan <youaryanyekrangi@gmail.com>
2026-04-27 17:15:36 +0300

    nan solved, saving as pkl, romanized_ngram remaining

--- cleaned from scratch.py
+++ cleaned from scratch.py
@@ -3,6 +3,10 @@ from text_utils import load_data
 from ngrams import ngram2dic, string2ngram, preprocess
 from uroman import Uroman
 from collections import Counter
+from time import perf_counter
+import pickle
+# VARIABLES
+N = 4
 
 # LOAD DATA FROM TXT FILES
 X_train = load_data('./WiLI-2018_dataset/x_train.txt')
@@ -18,19 +22,16 @@ df_train = pd.DataFrame({
     "target_language": y_train
 })
 
+start_t = perf_counter()
+df_train.insert(2, "romanized", df_train.text.apply(lambda text: romanizer.romanize_string(text)))
+end_t = perf_counter()
+print(end_t - start_t) # 2116.4 s = 35 m.
 
-df_rm = pd.read_csv('data_and_romanized.csv', encoding='utf-8')
-df_rm = df_rm.drop(df_rm.columns[0], axis=1)
-
-df_rm.insert(3,
-             'text_ngrams',
-             df_rm.text.apply(lambda text: ngram2dic(string2ngram(preprocess(text), 4))))
-
-df_rm.insert(4,
-             'romanized_ngrams',
-             df_rm.romanized.apply(lambda text: ngram2dic(string2ngram(preprocess(text), 4))))
+df_train.insert(3, "text_ngrams", df_train.text.apply(lambda text: ngram2dic(string2ngram(preprocess(text), N))))
+df_train.insert(4, "romanized_ngrams", df_train.text.apply(lambda text: ngram2dic(string2ngram(preprocess(text), N))))
 
 
+"""
 # READING FILE (900MB)
 import ast
 import time
@@ -47,9 +48,8 @@ print(end_t - start_t) # 382 s
 new_df_copy = new_df.copy()
 new_df_copy.text_ngrams = str_dic_text
 new_df_copy.romanized_ngrams = str_dic_romanized
-
+"""
 # TODO: remake combine dictionary function and import into text_utils.py
-# TODO: 1. define df, define lang, define column (romanized vs text ngrams)
 def combine(df, lang, col):
     lang_df = df[df.target_language==lang]
     total = Counter()
@@ -57,17 +57,16 @@ def combine(df, lang, col):
         total += Counter(lang_df[col].values[i])
     return total
 
-start_t = time.perf_counter()
+start_t = perf_counter()
 lang_text_all_ngrams = {}
-for lang in new_df_copy.target_language.unique():
-    lang_text_all_ngrams[lang] = combine(df=new_df_copy, lang=lang, col='text_ngrams')
+for lang in df_train.target_language.unique():
+    lang_text_all_ngrams[lang] = combine(df=df_train, lang=lang, col='text_ngrams')
 
 lang_romanized_all_ngrams = {}
-for lang in new_df_copy.target_language.unique():
-    lang_romanized_all_ngrams[lang] = combine(df=new_df_copy, lang=lang, col='romanized_ngrams')
-end_t = time.perf_counter()
-print(end_t - start_t)
-
+for lang in df_train.target_language.unique():
+    lang_romanized_all_ngrams[lang] = combine(df=df_train, lang=lang, col='romanized_ngrams')
+end_t = perf_counter()
+print(end_t - start_t) # 321 s
 
 
 # === TRAIN MODEL ===
@@ -75,18 +74,21 @@ from ngrams import NgramLanguageModel, train_language_models
 models_text = train_language_models(lang_text_all_ngrams, alpha=0.1)
 models_romanized = train_language_models(lang_romanized_all_ngrams, alpha=0.1)
 
-# FIX FOR NOW REMOVE LATER
-import math
-for k, v in models_text.items():
-    if isinstance(v, float) and math.isnan(v):
-        models_text[k] = 'nan'
 
 # creating testing data
 df_test = pd.DataFrame()
 df_test.insert(0, 'text', X_test)
 df_test.insert(1, 'target_language', y_test)
+# current quick fix
+df_test.iloc[41408] = [df_test.iloc[41408].text.replace('十分之', '分之'), df_test.iloc[41408].target_language]
 df_test.insert(2, 'ngrams', df_test.text.apply(lambda text: 
-                                     ngram2dic(string2ngram(text,4))))
+                                     ngram2dic(string2ngram(text, N))))
+df_test.insert(3, "romanized", df_test.text.apply(lambda text: romanizer.romanize_string(text)))
+
+
+df_test.to_pickle("df_test.pkl")
+df_new_test = pd.read_pickle("df_test.pkl")
+
 
 def predict_language(models, text):
     #trigrams = extract_trigrams(normalize(text))
@@ -105,11 +107,37 @@ predict_language(models=models_text, text=df_test.iloc[1,].ngrams)
 # THIS IS VERY SLOW
 results = []
 #for i in range(len(df_train)):
-for i in range(100):
+for i in range(10000):
     text_trigrams = df_test.iloc[i].ngrams
-    prediction = predict_language(models, text_trigrams)
+    prediction = predict_language(models_text, text_trigrams)
     label = df_test.iloc[i].target_language
     results.append([prediction, label])
 
+predictions = [result[0] for result in results]
+labels = [result[1] for result in results]
+from sklearn.metrics import accuracy_score
+accuracy_score(labels, predictions, sample_weight=None) #0.8796
 
-#TODO: 'nan' is not transferred into dataframes NaN
+start_t = perf_counter()
+df_train.to_pickle("df_train.pkl")
+end_t = perf_counter()
+print(end_t - start_t) # 42 s.
+
+start_t = perf_counter()
+df_new = pd.read_pickle("df_train.pkl")
+end_t = perf_counter()
+print(end_t - start_t) # 26 s.
+
+
+
+# AttributeError: 'Edge' object has no attribute 'value'
+bad_rows = []
+for i, text in enumerate(df_test["text"]):
+    try:
+        romanizer.romanize_string(text)
+    except Exception as e:
+        bad_rows.append((i, text, str(e)))
+        continue
+
+print("Total bad rows:", len(bad_rows))
+
